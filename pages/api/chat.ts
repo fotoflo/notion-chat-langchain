141cfd4bae076911b3d12df27ffc6181b8dc7480
Alex Miller <fotoflo@gmail.com>
2023-06-14 15:39:26 +0800

    tiny changes

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -20,6 +20,12 @@ export default async function handler(
     // OpenAI recommends replacing newlines with spaces for best results
     const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
 
+    const prompt = `You are a helpful bot that answers questions about Accelerating Asia.
+    The following is a conversation with a user. 
+    If there's a link in the doc, please put it in an <a> tag and append it to https://www.notion.so/acceleratingasia/ 
+    without the md extension.  Also who should i ask about, please include their contact info in the response
+    The user is a human and you are a bot. \n\nUser: ${sanitizedQuestion}\nBot:`;
+
     const pineconeArgs = {
       pineconeIndex: pinecone.Index(PINECONE_INDEX_NAME),
       namespace: PINECONE_NAME_SPACE,
@@ -37,7 +43,7 @@ export default async function handler(
 
     //Ask a question
     const response = await chain.call({
-      query: sanitizedQuestion,
+      query: prompt,
     });
 
     console.log('response', response);
